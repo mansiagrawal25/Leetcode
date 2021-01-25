12884216a9e82353a88fb73a31accdcab289954b
Mansi Agrawal <agrawal.ma@husky.neu.edu>
2021-01-25 18:20:28 -0500

    count digits

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,7 @@ public class Main {
 //Scanner scanner = new Scanner(System.in);
         int [] arr = {1,2,3,1};
         int k= 2;
+        int j =31;
         String [] str = {"flower","flee","fish"};
         Contains_Duplicate dup = new Contains_Duplicate();
         System.out.println(dup.containsDuplicate(arr));
@@ -19,7 +20,7 @@ public class Main {
         PowerOfTwo power = new PowerOfTwo();
         System.out.println(power.isPowerOfTwo(k));
         NumberOfDigitOne numbOne= new NumberOfDigitOne();
-        System.out.println(numbOne.countDigitOne(k));
+        System.out.println("Count of Digits is "+numbOne.countDigitOne(j));
         LongestCommonPrefix lcp =new LongestCommonPrefix();
         System.out.println(lcp.longestCommonPrefix(str));
         ShuffleArray shuffle = new ShuffleArray();
--- src/hard/NumberOfDigitOne.java
+++ src/hard/NumberOfDigitOne.java
@@ -2,22 +2,32 @@ package hard;
 
 public class NumberOfDigitOne {
     public int countDigitOne(int n) {
-        int count =1;
-if(n==0){
-    count=0;
-    return count;
-}
+//        int count =1;
+//if(n==0){
+//    count=0;
+//    return count;
+//}
+//
+//        for(int i=10; i<=n; i++){
+//
+//            String str = String.valueOf(i);
+//            for(int j=0; j<=str.length()-1;j++){
+////                char ch='1'
+//                if(str.charAt(j)=='1'){
+//                    count++;
+//                }
+//            }
+//        }
+//        return count;
 
-        for(int i=10; i<=n; i++){
+        int count = 0;
 
-            String str = String.valueOf(i);
-            for(int j=0; j<=str.length()-1;j++){
-//                char ch='1'
-                if(str.charAt(j)=='1'){
-                    count++;
-                }
-            }
+        for (long k = 1; k <= n; k *= 10) {
+            long r = n / k, m = n % k;
+            // sum up the count of ones on every place k
+            count += (r + 8) / 10 * k + (r % 10 == 1 ? m + 1 : 0);
         }
+
         return count;
     }
 }
